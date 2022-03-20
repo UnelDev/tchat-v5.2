74dbca744d3c7778dbf61fd87f6183f218bb478d
unel <86972822+UnelDev@users.noreply.github.com>
2022-03-20 18:00:59 +0100

    mise en place d'un tableaux pour réduire le code

--- client/chatBot/chatbotinteraction.cpp
+++ client/chatBot/chatbotinteraction.cpp
@@ -5,147 +5,23 @@ chatBotInteraction::chatBotInteraction()
 }
 bool chatBotInteraction::ImageSupported(const QString nameOfFile)
 {
-    bool state = false;
+    QList<QString>extentionList{"png","jpeg","JPEG","JPG","jp2","j2k","jpf","jpx","jpm","mj2","tif","tiff","gif","jpg","bmp"};
     const QString extention = nameOfFile.split(".").last();
-    if (extention == "png")
-    {
-        state = true;
+    if(extentionList.indexOf(extention)!=-1){
+        return true;
+    }else{
+        return false;
     }
-    if (extention == "jpeg")
-    {
-        state = true;
-    }
-    if (extention == "JPEG")
-    {
-        state = true;
-    }
-    if (extention == "JPG")
-    {
-        state = true;
-    }
-    // JPEG 2000
-    if (extention == "jp2")
-    {
-        state = true;
-    }
-    if (extention == "j2k")
-    {
-        state = true;
-    }
-    if (extention == "jpf")
-    {
-        state = true;
-    }
-    if (extention == "jpx")
-    {
-        state = true;
-    }
-    if (extention == "jpm")
-    {
-        state = true;
-    }
-    if (extention == "mj2")
-    {
-        state = true;
-    }
-    // TIFF
-    if (extention == "tif")
-    {
-        state = true;
-    }
-    if (extention == "tiff")
-    {
-        state = true;
-    }
-    // gif
-    if (extention == "gif")
-    {
-        state = true;
-    }
-    // webP
-    if (extention == "jpg")
-    {
-        state = true;
-    }
-    return state;
 }
 bool chatBotInteraction::textSuported(const QString nameOfFile)
 {
-    bool state = false;
+    QList<QString> extentionList{/*texte brut*/"txt",/*html*/"html","htm",/*text riche*/"md","css","xml",/*extention de progamation*/"h","hpp","c","cpp","js","py","bat","cmd","rs","rlib","java","cs" };
     const QString extention = nameOfFile.split(".").last();
-    //texte brut
-    if (extention == "txt")
-    {
-        state = true;
+    if(extentionList.indexOf(extention)!=-1){
+        return true;
+    }else{
+        return false;
     }
-    //texte mis en forme
-    if (extention == "html")
-    {
-        state = true;
-    }
-    if (extention == "htm")
-    {
-        state = true;
-    }
-    if (extention == "md")
-    {
-        state = true;
-    }
-    if (extention == "css")
-    {
-        state = true;
-    }
-    if (extention == "xml")
-    {
-        state = true;
-    }
-    //extention de programation
-    if (extention == "h")
-    {
-        state = true;
-    }
-    if (extention == "hpp")
-    {
-        state = true;
-    }
-    if (extention == "c")
-    {
-        state = true;
-    }
-    if (extention == "cpp")
-    {
-        state = true;
-    }
-    if (extention == "js")
-    {
-        state = true;
-    }
-    if (extention == "py")
-    {
-        state = true;
-    }
-    if (extention == "bat")
-    {
-        state = true;
-    }
-    if (extention == "cmd")
-    {
-        state = true;
-    }
-    if (extention == "rs")
-    {
-        state = true;
-    }
-    if (extention == "rlib")
-    {
-        state = true;
-    }
-    if (extention == "java")
-    {
-        state = true;
-    }
-    
-    return state;
 }
 QString chatBotInteraction::exctractText(const QString nameOfFile, const int nbOfLinePrint)
 {
